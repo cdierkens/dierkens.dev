a20d7065a9a73d0eb3a274a1885d0460968f621f
Christopher Dierkens <christopher.dierkens@formidable.com>
2020-11-27 14:44:02 -0700

    remove container styles

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Container: React.FC = ({ children }) => (
-  <div className="container max-w-xl mx-auto px-2 my-6">{children}</div>
-);
+const Container: React.FC = ({ children }) => <div>{children}</div>;
 
 export default Container;
