dd7bc9435d9f78e1203550110f7eeb2dd77ae61d
Christopher Dierkens <cjdierkens@gmail.com>
2020-12-17 19:33:34 -0700

    mock the date for a stable test

--- src/components/BlogPreview/BlogPreview.test.tsx
+++ src/components/BlogPreview/BlogPreview.test.tsx
@@ -1,18 +1,21 @@
 import { render } from "@testing-library/react";
+import mockdate from "mockdate";
 import React from "react";
 import BlogPreview from "./BlogPreview";
 
 it("matches snapshot", () => {
+  mockdate.set("2020-02-01");
   const { asFragment } = render(
     <BlogPreview
       title="Does drinking coffee make you a better developer?"
       description="Alcatra venison sirloin, dolore magna cupidatat boudin nostrud. Eiusmod ut prosciutto, fatback ipsum leberkas bresaola beef ribs. Porchetta exercitation pastrami kevin frankfurter aliqua. Burgdoggen minim rump dolore swine, shoulder velit ball tip drumstick aute eiusmod adipisicing pancetta strip steak non. Kielbasa tri-tip id beef ribs chuck chicken."
       author="Christopher Dierkens"
-      date="Sunday, March 1, 2020"
+      date="2020-01-01"
       avatar="/images/avatar-dierkens-chris.jpg"
       href="mock-href"
     />
   );
 
   expect(asFragment()).toMatchSnapshot();
+  mockdate.reset();
 });
