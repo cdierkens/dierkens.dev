39bd64b9c3a5c9f760c42778ff24cd42e7f1cf70
Christopher Dierkens <christopher.dierkens@formidable.com>
2021-05-25 23:13:22 -0400

    jest 17

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  transform: {
-    "^.+\\.[jt]sx?$": "<rootDir>/jest.preprocess.js",
-  },
+  // transform: {
+  //   "^.+\\.[jt]sx?$": "<rootDir>/jest.preprocess.js",
+  // },
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{js,jsx,ts,tsx}",
     "!**/node_modules/**",
@@ -11,6 +11,7 @@ module.exports = {
     ".+\\.(css|styl|less|sass|scss)$": `<rootDir>/__mocks__/identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
+  testEnvironment: "jsdom",
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
@@ -21,5 +22,6 @@ module.exports = {
   setupFilesAfterEnv: [
     "@testing-library/jest-dom/extend-expect",
     "<rootDir>/jest.matchMedia.js",
+    "<rootDir>/jest.immediate.js",
   ],
 };
