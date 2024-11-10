e98c84340f7867a7c7ba446df914cef58d20a161
Christopher Dierkens <cjdierkens@gmail.com>
2024-11-10 14:51:28 -0500

    feat: better link previews

--- apps/dierkens.dev/src/consts.ts
+++ apps/dierkens.dev/src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "dierkens.dev";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "👋 Hi I'm Chris. Checkout my website for software engineering content.";
